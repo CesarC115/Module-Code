bb6a0114b0813158ec05e640b7a5838eb20e6380
CesarC115 <cesar.tcorral115@gmail.com>
2024-02-27 12:06:51 -0700

    chack 2

--- module01.hello.world/src/main.cpp
+++ module01.hello.world/src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <wpi-32u4-lib.h>
+#include <Romi32U4Buttons.h>
 
 // TODO: Add some header information here.  Names, date, etc.    Add comments as you go. Future you will thank you.
+//Cesar Corral  1/23/2024
 
 // TODO: declare the pin for the LED
+#define  LED1 PC7 //YEllow LED
+#define LED_PIN 13 //built oin led
 
 
 // Create a button object for the built-in Button A on the Romi
 Romi32U4ButtonA buttonA;
+Romi32U4ButtonB btnB;
+Romi32U4ButtonC btnC;
 
 // Define two basic states. For this program, they will correspond to an LED state (on or off).
 // "enum" stands for "enumerate". Basically, we define a new variable type called ROBOT_STATE.
@@ -16,6 +22,8 @@ enum ROBOT_STATE {ROBOT_IDLE, ROBOT_ACTIVE};
 
 // Declare a variable, robotState, of our new type, ROBOT_STATE. Initialize it to ROBOT_IDLE.
 ROBOT_STATE robotState = ROBOT_IDLE;
+int countIDLE = 0;
+int countACTIVE = 0;
 
 
 
@@ -25,10 +33,13 @@ void handleButtonPress(void)
   if(robotState == ROBOT_IDLE)
   {
       // TODO: Notify us that we're switching to ACTIVE using the serial print
+      Serial.print("ACTIVE MODE\n");
       // TODO: Count code?
-
+      countACTIVE++;
+      Serial.println(countACTIVE);
       // TODO: Turn the LED on
-      
+      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(LED_PIN, LOW);
 
       // Finally, update the state
       robotState = ROBOT_ACTIVE;
@@ -38,11 +49,14 @@ void handleButtonPress(void)
   else if(robotState == ROBOT_ACTIVE)
   {
       // TODO: Notify us that we're switching to IDLE
-      
+      Serial.print("IDLE MODE\n");
+
       // BONUS TODO: Output your team names
+      Serial.print("Cesar Corral Bryan\n");
 
       // TODO: Turn the LED off
-      
+      digitalWrite(LED_BUILTIN, HIGH);
+      digitalWrite(LED_PIN, HIGH);
 
       // Finally, update the state
       robotState = ROBOT_IDLE;
@@ -57,10 +71,11 @@ void setup()
 {
   // TODO: Initialize the Serial at a baud rate of 115200 for prints
   // Be sure to set your Serial Monitor appropriately in platformio.ini
-  
+  Serial.begin(115200);
 
   // TODO: Set the LED pin to be an OUTPUT
-  
+  pinMode(LED_PIN,  OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 /* Here is where all the fun happens. For each state, check for and respond to a button press.
@@ -68,4 +83,6 @@ void setup()
 void loop()
 {
   if(buttonA.getSingleDebouncedPress()) handleButtonPress();
+  else if(btnB.getSingleDebouncedPress()) handleButtonPress();
+  else if(btnC.getSingleDebouncedPress()) handleButtonPress();
 }
--- module02.moveit/src/main.cpp
+++ module02.moveit/src/main.cpp
@@ -3,6 +3,7 @@
  */ 
 
 //TODO, Add your group information to the top of your code.
+//Cesar Corral Bryan Amato
 
 #include <Arduino.h>
 #include <wpi-32u4-lib.h>
@@ -10,14 +11,20 @@
 #include <IRdecoder.h>
 #include <ir_codes.h>
 
+void handleMotionComplete(); 
+
 // TODO, Section, 4.2: Add line to include Chassis.h
+#include <Chassis.h>
 
 // Sets up the IR receiver/decoder object
 const uint8_t IR_DETECTOR_PIN = 1;
 IRDecoder decoder(IR_DETECTOR_PIN);
 
 // TODO, Section 4.2: Declare the chassis object (with default values)
+//Chassis chassis(7,1440,14.7);
+
 // TODO, Section 6.2: Adjust parameters to better match actual motion
+Chassis chassis(7,1440,14);
 
 // A helper function for debugging
 #define LED_PIN 13
@@ -34,15 +41,15 @@ ROBOT_STATE robotState = ROBOT_IDLE;
 // idle() stops the motors
 void idle(void)
 {
-  Serial.println("idle()");
+  Serial.println("idle() state");
   setLED(LOW);
 
   // TODO, Section 4.2: Uncomment call to chassis.idle() to stop the motors
-  // chassis.idle();
+  chassis.idle();
 
   //set state to idle
   robotState = ROBOT_IDLE;
-  Serial.println("/idle()");
+  // Serial.println("idle()");
 }
 
 /*
@@ -56,9 +63,9 @@ void setup()
   Serial.begin(115200);
 
   // TODO, Section 4.2: Initialize the chassis (which also initializes the motors)
-
+  chassis.init();
   // TODO, Section 5.1: Adjust the PID coefficients
-
+  chassis.setMotorPIDcoeffs(5,0.1);
   idle();
 
   // Initializes the IR decoder
@@ -70,15 +77,16 @@ void setup()
 // A helper command to drive a set distance
 // At the start, it will take no arguments and we'll hardcode a motion
 // TODO, Section 6.1 (but not before!): Edit the function definition to accept a distance and speed
-void drive(void)
+void drive(float distancecm, float speed)
 {
   setLED(HIGH);
   robotState = ROBOT_DRIVE_FOR;
 
   // TODO: In Section 4.2 and 5.1, add a call to chassis.setWheelSpeeds() to set the wheel speeds
+  //chassis.setWheelSpeeds(30,30);
 
   // TODO: In Section 6.1, remove the call to setWheelSpeeds() and add a call to chassis.driveFor()
-
+  chassis.driveFor(distancecm,speed,false);
 }
 
 // A helper function to turn a set angle
@@ -88,25 +96,41 @@ void turn(float ang, float speed)
   robotState = ROBOT_DRIVE_FOR;
 
   // TODO, Section 6.1: Make a call to chassis.turnFor()
-
+  chassis.turnFor(ang, speed);
 }
 
 // TODO, Section 6.1: Declare function handleMotionComplete(), which calls idle()
+void handleMotionComplete(){
+  idle();
+} 
 
 // Handles a key press on the IR remote
 void handleKeyPress(int16_t keyPress)
 {
-  Serial.println("Key: " + String(keyPress));
-
-  // TODO, Section 3.2: add "emergency stop"
+  //println("Key: " + String(keyPress));
 
   switch(robotState)
   {
     case ROBOT_IDLE:
       // TODO, Section 3.2: Handle up arrow button
-
-
+      if(keyPress == UP_ARROW){
+        drive(40.0,35.0);
+        Serial.print("Button UP ARROW");
+      }
       // TODO, Section 6.1: Handle remaining arrows
+      if(keyPress == RIGHT_ARROW){
+        turn(-90, 80);
+        Serial.print("Button RIGHT ARROW");
+      }
+      if(keyPress == LEFT_ARROW){
+        turn(90,80);
+        Serial.print("Button LEFT ARROW");
+      }
+      if(keyPress == DOWN_ARROW){
+        drive(-20.0,20.0);
+        Serial.print("Button DOWN ARROW");
+      }
+    
       break;
       
     default:
@@ -129,12 +153,21 @@ void loop()
   switch(robotState)
   {
     case ROBOT_DRIVE_FOR: 
+      //Printing Speed of Wheels
+     // chassis.printSpeeds();
+      
 
+      if(keyPress == ENTER_SAVE){
+        idle();
+        Serial.print("Idle key pressed");
+      }
+      
       // TODO, Section 6.1: Uncomment to handle completed motion
-      // if(chassis.checkMotionComplete()) handleMotionComplete(); 
+      if(chassis.checkMotionComplete()) handleMotionComplete(); 
       break;
 
     default:
+      robotState = ROBOT_IDLE;
       break;
   }
 }
